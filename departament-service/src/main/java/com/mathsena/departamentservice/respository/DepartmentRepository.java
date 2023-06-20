8a74c031f5c12b9b9c952e2ef740bd27d8d653be
MathSena <MathSena07@hotmail.com>
2023-06-20 11:05:48 +0100

    Initial commit

---
+++ departament-service/src/main/java/com/mathsena/departamentservice/respository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package com.mathsena.departamentservice.respository;
+
+
+import com.mathsena.departamentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
