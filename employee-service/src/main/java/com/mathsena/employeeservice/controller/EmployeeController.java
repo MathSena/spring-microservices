7db056e1c1d61d21ad5f686c156f3553f3620411
MathSena <MathSena07@hotmail.com>
2023-06-30 00:24:35 +0100

    Generate REST API Documentation for Employee Service

--- employee-service/src/main/java/com/mathsena/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/mathsena/employeeservice/controller/EmployeeController.java
@@ -3,11 +3,18 @@ package com.mathsena.employeeservice.controller;
 import com.mathsena.employeeservice.dto.APIResponseDto;
 import com.mathsena.employeeservice.dto.EmployeeDto;
 import com.mathsena.employeeservice.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Employee Service - Employee Controller",
+        description = "Employee Controller Exposes REST APIs for Employee-Service"
+)
 @RestController
 @RequestMapping("api/employees")
 @AllArgsConstructor
@@ -19,6 +26,14 @@ public class EmployeeController {
 
     //Build Save Employee REST API
 
+    @Operation(
+            summary = "POST Employee REST API",
+            description = "Save a new Employee REST API is used to save Employee object in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto){
         EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto);
@@ -27,6 +42,14 @@ public class EmployeeController {
 
     // Build Get Employee REST API
 
+    @Operation(
+            summary = "GET Employee REST API",
+            description = "Get a new Employee REST API is used to get a Employee object in a database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("{id}")
     public ResponseEntity<APIResponseDto> getEmployee(@PathVariable("id") Long employeeId){
         APIResponseDto employeeDto = employeeService.getEmployeeById(employeeId);
--- employee-service/src/main/java/com/mathsena/employeeservice/dto/EmployeeDto.java
+++ employee-service/src/main/java/com/mathsena/employeeservice/dto/EmployeeDto.java
@@ -1,19 +1,38 @@
 package com.mathsena.employeeservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "EmployeeDto Model Information"
+)
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeDto {
     private Long id;
-    private  String firstName;
+    @Schema(
+            description = "Employee firstName"
+    )
+    private String firstName;
+    @Schema(
+            description = "Employee lastName"
+    )
     private String lastName;
+    @Schema(
+            description = "Employee email"
+    )
     private String email;
+    @Schema(
+            description = "Department Code"
+    )
     private String departmentCode;
+    @Schema(
+            description = "Organization Code"
+    )
     private String organizationCode;
 }
