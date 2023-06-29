3dfcb7c84ae7c03ad0c3fe5fae1e126b4a535cc2
MathSena <MathSena07@hotmail.com>
2023-06-29 23:49:19 +0100

    Department-Service - REST API Documentation

--- departament-service/src/main/java/com/mathsena/departamentservice/DepartamentServiceApplication.java
+++ departament-service/src/main/java/com/mathsena/departamentservice/DepartamentServiceApplication.java
@@ -1,9 +1,30 @@
 package com.mathsena.departamentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Department Service REST APIs",
+                description = "Department Service REST APIs Documentation",
+                version="v1.0",
+                contact = @Contact(
+                        name = "Matheus Sena",
+                        email = "mathsena07@hotmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Department-Service Doc"
+        )
+)
 @SpringBootApplication
 public class DepartamentServiceApplication {
 
